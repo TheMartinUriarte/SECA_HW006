e02d0e0ae6826c1a45bdbdb3fa5f58b71aec4301
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-03-21 10:52:30 -0700

    [Check] Sanity check

---
+++ spring-boot-microservices/songs-api/src/main/resources/db/migration/V4__Add_length_column.sql
@@ -0,0 +1,2 @@
+alter table SONGS
+    add column SONG_LENGTH varchar(100) NOT NULL;
---
+++ spring-boot-microservices/songs-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO SONGS
+    (TITLE, SONG_LENGTH)
+VALUES
+   ('Kachi Daze', '7:38'),
+   ('In the Face of Evil', '6:49'),
+   ('All The Stars', '3:52'),
+   ('My Shot', '5:33');
